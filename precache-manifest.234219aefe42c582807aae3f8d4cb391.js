c06c307d05523e250b605279b4285cba0fb7e7e5
QuePark <gyuhapark11@gmail.com>
2020-07-07 12:29:49 +0900

    Updates

---
+++ precache-manifest.234219aefe42c582807aae3f8d4cb391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287387e88b63231fdb9a7c78029d2b5e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "72a479117748afa485ac",
+    "url": "/movie_app/static/css/main.7262f2d7.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a479117748afa485ac",
+    "url": "/movie_app/static/js/main.d83cf7cb.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
